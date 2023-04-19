2877a702d4552935fbc9b686a8027535644ad257
Quentin Laffont <quentin@flexper.fr>
2023-04-19 13:17:08 +0200

    feat(app): add favicon

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -59,7 +59,9 @@ export const AppLayout = ({ children }: React.PropsWithChildren) => {
       <div className="w-full py-6 shadow-2xl">
         <div className="container m-auto flex flex-wrap items-center justify-center gap-4 px-4 sm:justify-between">
           <div>
-            <i className="brand icon-logo block h-6 w-36 bg-black dark:bg-white"></i>
+            <Link href="/" className="hover:opacity-70">
+              <i className="brand icon-logo block h-6 w-36 bg-black dark:bg-white"></i>
+            </Link>
           </div>
 
           {isConnected ? (
