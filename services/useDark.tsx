963282c83efbcb742ecbd2071e7793885ba0e9a8
Quentin Laffont <quentin@flexper.fr>
2023-04-25 08:56:59 +0200

    fix(dark-mode): fix ssr

--- services/useDark.tsx
+++ services/useDark.tsx
@@ -1,32 +1,46 @@
-import { useEffect } from 'react';
-import { useLocalStorage } from 'usehooks-ts';
+import { useEffect, useState } from 'react';
+import { useSsr } from 'usehooks-ts';
 
 const COLOR_SCHEME_QUERY = '(prefers-color-scheme: dark)';
 
+const getPrefersScheme = (defaultValue?: boolean): boolean => {
+  if (localStorage.getItem('darkMode')) {
+    return localStorage.getItem('darkMode') === 'true';
+  }
+
+  // Prevents SSR issues
+  if (typeof window !== 'undefined') {
+    return window.matchMedia(COLOR_SCHEME_QUERY).matches;
+  }
+
+  return !!defaultValue;
+};
+
 export const useDark = (defaultValue?: boolean) => {
-  const getPrefersScheme = (): boolean => {
-    // Prevents SSR issues
-    if (typeof window !== 'undefined') {
-      return window.matchMedia(COLOR_SCHEME_QUERY).matches;
+  const { isBrowser } = useSsr();
+  // const [isDarkMode, setDarkMode] = useLocalStorage<boolean>('darkMode', false);
+  const [isDarkMode, setDarkMode] = useState<boolean>(undefined);
+
+  useEffect(() => {
+    if (isBrowser) {
+      setDarkMode(getPrefersScheme(defaultValue));
     }
-
-    return !!defaultValue;
-  };
-
-  const [isDarkMode, setDarkMode] = useLocalStorage<boolean>(
-    'darkMode',
-
-    getPrefersScheme(),
-  );
+  }, [isBrowser]);
 
   useEffect(() => {
     if (document) {
       if (isDarkMode) {
         document.documentElement.classList.add('dark');
         document.documentElement.setAttribute('data-color-mode', 'dark');
+        if (isBrowser) {
+          localStorage.setItem('darkMode', 'true');
+        }
       } else {
         document.documentElement.classList.remove('dark');
         document.documentElement.setAttribute('data-color-mode', 'light');
+        if (isBrowser) {
+          localStorage.setItem('darkMode', 'false');
+        }
       }
     }
   }, [isDarkMode]);
